6b6028b0f825838b102ce49c7923fe5ca141104d
David <iordadavid@yahoo.com>
2025-07-21 12:13:07 +0300

    t&cs + gdpr + faq + minor adjustments

---
+++ Scripts/faq.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(item => {
+    const question = item.querySelector('.faq-question');
+
+    question.addEventListener('click', () => {
+      item.classList.toggle('active');
+
+      const answer = item.querySelector('.faq-answer');
+      if (item.classList.contains('active')) {
+          answer.style.maxHeight = answer.scrollHeight + "px";
+      } else {
+          answer.style.maxHeight = "0";
+      }
+  });});
+});
